3779a65c67584d17df732f872aba1ba7fbd3b55e
Delonely4 <ildar199718@icloud.com>
2025-08-20 13:26:44 +0300

    chore

--- index.js
+++ index.js
@@ -1,123 +1,76 @@
 console.log("Виселица");
-let gameRunning = true;
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const arr = ["покой", "голод", "кровопролитный", "талант", "усердие"];
-for (let i = 0; i < arr.length; i++) {
-  console.log("Слово " + (i + 1) + ": " + arr[i]);
-}
+const secretWord = "порка";
+const secretWordArr = secretWord.split("");
+const guessedWord = new Array(secretWord.length).fill("_");
 
-function showWord(displayArr) {
-  return displayArr.join(" ");
-}
+let hp = 5;
+let gameRunning = true;
 
-function playHangman() {
-  console.log("==ИГРА НАЧАЛАСЬ==");
-  let randomIndex = Math.floor(Math.random() * arr.length);
-  let currentWord = arr[randomIndex];
+console.log("==Начать Игру==");
+console.log("Угадай слово из ", secretWord.length, "букв.");
+console.log("Тебе попалось слово " + guessedWord.join(" "));
+console.log("У вас есть ", hp);
 
-  let displayWord = [];
-  for (let i = 0; i < currentWord.length; i++) {
-    displayWord.push("_");
-  }
+function isGuess(letter) {
+  let letterFound = false;
 
-  console.log("Загаданное слово из " + currentWord.length + " букв");
-  console.log(showWord(displayWord));
-  console.log("Игра окончена");
-}
-
-function checkLetter(Letter, word, displayArr) {
-  let found = false;
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] === Letter) {
-      displayArr[i] = Letter;
-      found = true;
+  for (let i = 0; i < secretWord.length; i++) {
+    if (secretWordArr[i] === letter) {
+      guessedWord[i] = letter;
+      letterFound = true;
     }
   }
-  return found;
-}
 
-function startGame() {
-  while (gameRunning) {
-    console.log("==МЕНЮ==");
-    console.log("1. Играть");
-    console.log("2. Выйти");
-
-    let choice = "1";
-
-    if (choice === "1") {
-      playHangman();
-      console.log("Возвращаемся в меню");
-      choice = "2";
-    }
-
-    if (choice === "2") {
-      console.log("До свидания!");
-      gameRunning = false;
-    }
+  if (letterFound) {
+    console.log(guessedWord.join(" "));
+  } else {
+    console.log("Неверно!");
+    hp--;
+    printGallow();
   }
 }
-startGame();
 
-//   if (choice === "1") {
-//     console.log("Начинаем игру...");
-//   } else if (choice === "2") {
-//     console.log("До свидания!");
-//     gameRunning = false;
-//   }
-//   const arr = ["покой", "голод", "кровопролитный", "талант", "усердие"];
-// }
+function printGallow() {
+  if (hp--) {
+    console.log("ОШИБКА");
+    console.log("");
+  }
+}
 
-// console.log("hello world");
-// function calculator(a, b, c) {
-//   console.log("a", a);
-//   console.log("b", b);
-//   console.log("c", c);
-//   const d = 5;
-//   if (b == "-") {
-//     console.log(a - c);
-//   }
-//   if (b == "+") {
-//     console.log(a + c);
-//   }
-//   if (b == "/") {
-//     console.log(a / c);
-//   }
-//   if (b == "*") {
-//     console.log(a * c);
-//   }
+function isGame() {
+  if (guessedWord.join("") === secretWord) {
+    console.log("Вы выиграли!", "Слово - ", secretWord);
+    gameRunning = false;
+  }
 
-//   switch (b) {
-//     case "+":
-//       console.log(a + c);
-//       break;
-//     case "-":
-//       console.log(a - c);
-//       break;
-//     case "/":
-//       console.log(a / c);
-//       break;
-//     case "*":
-//       console.log(a * c);
-//       break;
-//     default:
-//       console.log("not supported");
-//   }
-// }
-// calculator(6, "*", 3);
-// const arr = [0, 1, 2, 3, 4, 5];
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i] * 2);
-// }
-// const arr = [];
-// for (let i = 0; i < arr.length; i++) {}
-// console.log(arr[0]);
+  if (hp > 0) {
+    processStep();
+  }
 
-// const arr = [0, 1, 2, 3, 4, 5];
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i] * 2);
-// }
+  if (hp <= 0) {
+    console.log("Вы проиграли!");
+    gameRunning = false;
+    rl.close();
+  }
+}
+
+function processStep() {
+  if (gameRunning) {
+    rl.question("Введите букву: ", (answer) => {
+      console.log(answer);
+      const letter = answer;
+      isGuess(letter);
+      isGame();
+    });
+  } else {
+    rl.close();
+  }
+}
+
+processStep();
